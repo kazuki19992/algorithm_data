6ea64eed664838b191a9aa048b97aed9fdde5863
kazuki19992 <kushida98@gmail.com>
2019-05-07 00:24:26 +0900

    正常動作

--- 03-a-05.c
+++ 03-a-05.c
@@ -3,6 +3,7 @@
 //2019/05/06 17:09
 
 #include<stdio.h>
+#include<assert.h>
 #define MAXNUM 10
 int main(){
     int nin;
@@ -12,11 +13,13 @@ int main(){
 
     
     scanf("%d",&nin); //nin → &nin
+    assert(nin > 0); //想定値以外の入力を禁止
     for(i=0;i<nin;i++){
         scanf("%d",&data[i]);
+        assert(data[i] >= 0 && data[i] <= 100); //想定値以外の入力を禁止
     }
 
-    pdata = &data[0]; //*data → &data[0]a.
+    pdata = &data[0]; //*data → &data[0]
 
     for(i=0;i<nin;i++){
         *pdata++=*(data+i)+43;
@@ -25,7 +28,7 @@ int main(){
     pdata=&data[0];
     goukei=*pdata; //pdata → *pdata
 
-    for(i=0;i<=nin;i++){ //i=1 → i=0
+    for(i=1;i<nin;i++){ //i=1 → i=0 // i<=nin → i<nin
         goukei += *( pdata + i );  //pdata → ( pdata + i )
     }
     printf("合計=%d\n",goukei);
