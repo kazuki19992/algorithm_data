166fbc1efc923792fa036c48f16d0f85288ab204
kazuki19992 <kushida98@gmail.com>
2019-05-06 21:54:25 +0900

    更新

---
+++ 03-a-05.c
@@ -0,0 +1,34 @@
+//ポインタ
+//u306065
+//2019/05/06 17:09
+
+#include<stdio.h>
+#define MAXNUM 10
+int main(){
+    int nin;
+    int data[MAXNUM],*pdata;
+    int goukei;
+    int i;
+
+    //printf("人数入力");
+    scanf("%d",&nin); //nin → &nin
+    for(i=0;i<nin;i++){
+        //printf("素点");
+        scanf("%d",&data[i]);
+    }
+
+    pdata = &data[0]; //*data → &data[0]a.
+
+    for(i=0;i<nin;i++){
+        *pdata++=*(data+i)+43;
+    }
+
+    pdata=&data[0];
+    goukei=*pdata; //pdata → *pdata
+
+    for(i=0;i<=nin;i++){ //i=1 → i=0
+        goukei += *( pdata + i );  //pdata → ( pdata + i )
+    }
+    printf("合計=%d\n",goukei);
+    return(0);
+}
